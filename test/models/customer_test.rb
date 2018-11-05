33b76bd720d6349fd94dc5c609817bc2e11bca42
Sigrid Benezra <sdbenezra@gmail.com>
2018-11-05 15:59:02 -0800

    Add test for customer relation to rentals. Fix line in test_helper

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -6,4 +6,14 @@ describe Customer do
   it "must be valid" do
     value(customer).must_be :valid?
   end
+
+  describe "relations" do
+    it "has a list of rentals" do
+      customer = customers(:one)
+      customer.must_respond_to :rentals
+      customer.rentals.each do |rental|
+        rental.must_be_kind_of Rental
+      end
+    end
+  end
 end
