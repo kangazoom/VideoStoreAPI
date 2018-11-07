8c052b60b728b4bf88bebbb1be162177f5c2af8b
Sigrid Benezra <sdbenezra@gmail.com>
2018-11-07 14:49:55 -0800

    Rework checkin method to use post. Tests passing for all but inventory under checkin.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -14,22 +14,40 @@ class RentalsController < ApplicationController
     # movie_id = params[:movie_id].to_i
     # movie = Movie.find_by(id: movie_id)
     movie = Movie.find_by(id: rental_params[:movie_id])
+    customer = Customer.find_by(id: rental_params[:customer_id])
     # customer_id = params[:customer_id].to_i
     # customer = Customer.find_by(id: customer_id)
-    rental = Rental.new(rental_params)
-    # rental = Rental.new(movie_id: movie_id, customer_id: customer_id, check_out: check_out, due_date: due_date)
-
-    if rental.save
-      movie.calculate_available_inventory
-      render json: rental.as_json(except: [:created_at, :updated_at]), status: :ok
-
+    if movie == nil || customer == nil
+      render json: {errors: "No movie checked out, missing movie or customer"}, status: :bad_request
     else
-      render_error(:bad_request, rental.errors.messages)
+      if movie.available_inventory > 0
+        rental = Rental.new(rental_params)
+        if rental.save
+          movie.calculate_available_inventory
+          render json: rental.as_json(except: [:created_at, :updated_at]), status: :ok
+        else
+          render_error(:bad_request, rental.errors.messages)
+        end
+      else
+        render json: {errors: "No available movies in inventory"}, status: :ok
+      end
     end
   end
 
   def checkin
-    
+    movie = Movie.find_by(id: rental_params[:movie_id])
+    customer = Customer.find_by(id: rental_params[:customer_id])
+    if movie == nil || customer == nil
+      render json: {errors: "Not checked in. Customer or Movie id is invalid"}, status: :not_found
+    else
+      rental = Rental.find_by(movie_id: movie.id, customer_id: rental_params[:customer_id], checkedout: true)
+      if rental.update(checkedout: false)
+        movie.calculate_available_inventory
+        render json: rental.as_json(except: [:created_at, :updated_at]), status: :ok
+      else
+        render_error(:not_found, rental.errors.messages)
+      end
+    end
   end
 
 
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -14,11 +14,12 @@ describe RentalsController do
       expect(Rental.count).must_equal 3
       must_respond_with :success
       expect(Movie.first.available_inventory).must_equal 0
+      expect(Rental.last.checkedout).must_equal true
     end
 
 
     it "returns an error for invalid rental data" do
-      expect {
+      expect{
       post checkout_path, params: { movie_id: nil, customer_id: Customer.first.id }
     }.wont_change "Rental.count"
 
@@ -29,21 +30,53 @@ describe RentalsController do
 
       must_respond_with :bad_request
     end
+
+    it "returns an unavailable error message if available inventory is 0" do
+      movie = Movie.first
+      movie.update(available_inventory: 0)
+      expect{
+      post checkout_path, params: { movie_id: Movie.first.id, customer_id: Customer.first.id }
+    }.wont_change "Rental.count"
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_kind_of Hash
+    expect(body).must_include "errors"
+
+    must_respond_with :success
+
+    end
   end
 
-  # describe "rentals check_in" do
-  #   it "successfully changes checked_out status to false" do
-  #     expect {
-  #     post check_out_path, params: { movie_id: nil, customer_id: customers.first.id }
-  #
-  #     body = JSON.parse(response.body)
-  #     expect(body).must_be_kind_of Hash
-  #     expect(body).must_include "id"
-  #
-  #     rental = Rental.find(body["id"].to_i)
-  #
-  #     expect(rental.checkout_status).must_equal false
-  #     must_respond_with :success
-  #   end
-  # end
+  describe "rentals check_in" do
+    let(:rental_data) {
+          {
+            movie_id: Movie.first.id,
+            customer_id: Customer.first.id,
+          }
+        }
+    it "successfully changes checked_out status to false" do
+      post checkout_path, params: rental_data
+      expect {
+      post checkin_path, params: rental_data }.wont_change "Rental.count"
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+      expect(Rental.last.checkedout).must_equal false
+      must_respond_with :success
+    end
+
+    it "successfully adds movie back to available inventory" do
+      post checkout_path, params: rental_data
+      expect{post checkin_path, params: rental_data}.must_change "Movie.first.available_inventory", 1
+    end
+
+    it "returns not found if trying to check in a movie without a valid customer or movie id" do
+
+      post checkin_path, params: { movie_id: nil, customer_id: Customer.first.id }
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+    end
+  end
 end
