dc00864d56ff659333e3f82d5f17170116a06ebd
kangazoom <michellelcronin@gmail.com>
2018-11-07 09:53:35 -0800

    controller tests for movie: #index + #show

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,22 +1,59 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  def check_response(expected_type:, expected_status: :success)
+    must_respond_with expected_status
+    expect(response.header['Content-Type']).must_include 'json'
+
+    body = JSON.parse(response.body)
+    expect(body).must_be_kind_of expected_type
+    return body
+  end
+
   describe "index" do
-    it "succeeds for existing users" do
+    MOVIE_FIELDS_INDEX = %w(id title release_date).sort
+
+    it "successfully returns a list of existing movies" do
       get movies_path
-      must_respond_with :success
+
+      body = check_response(expected_type: Array)
+
+      expect(body.length).must_equal Movie.count
+
+      body.each do |movie|
+        expect(movie.keys.sort).must_equal MOVIE_FIELDS_INDEX
+      end
     end
 
-    it "succeeds when there are no movies" do
-      Movie.all.each do |movie|
-        movie.destroy
-      end
-
+    it "returns an empty array when there are no movies" do
+      Movie.destroy_all
       get movies_path
-      must_respond_with :success
+      body = check_response(expected_type: Array)
+      expect(body).must_equal []
     end
   end
+
+  describe 'show' do
+        MOVIE_FIELDS_SHOW = %w(id title overview release_date inventory available_inventory).sort
+    it 'retrieves info on one movie' do
+      movie = Movie.first
+      get movie_path(movie)
+
+      body = check_response(expected_type: Hash)
+      expect(body.keys.sort).must_equal MOVIE_FIELDS_SHOW
+    end
+
+    it 'returns a 404 error if requesting a movie that does not exist' do
+      movie = Movie.first
+      movie.destroy
+
+      get movie_path(movie)
+      body = check_response(expected_type: Hash, expected_status: :not_found)
+      expect(body).must_include 'errors'
+    end
+  end
+
+#   describe 'create' do
+#     TODO
+#   end
 end
