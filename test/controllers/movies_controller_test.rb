d429ff6f604da07b96b01162e6b55d9d1c36c93f
kangazoom <michellelcronin@gmail.com>
2018-11-09 05:52:25 -0800

    finished controller tests for Movies#creates; all passing

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -62,59 +62,31 @@ describe MoviesController do
         }
 
     it "creates a movie with valid data" do
-      # new_movie_params = {
-      #   title: 'good new movie',
-      #   release_date: '1978-01-2'
-      # }
 
       before_count = Movie.count
 
 
       post movies_path, params: movie_data
-      must_respond_with :success
+      body = check_response(expected_type: Hash, expected_status: :ok)
+
       expect(Movie.count).must_equal (before_count+1)
-
       expect(Movie.last.title).must_equal 'good new movie'
-
-
-      # new_movie_params = new_movie_params.to_json
-
-      # expect {
-      #   post movies_path, params: new_movie_params
-      # }.must_change 'Movie.count', 1
-      #
-      # new_movie = Movie.find_by(title: 'good new movie')
-      #
-      # # get movie_path(movie)
-      # body = check_response(expected_type: Hash, expected_status: :ok)
-      # expect(body).must_include 'errors'
     end
-    # #
-    # it "renders bad_request and does not update the DB for bogus data" do
-    #   new_work_params[:work][:title] = nil
-    #
-    #   expect {
-    #     post works_path, params: new_work_params
-    #   }.wont_change 'Work.count'
-    #
-    #   expect(flash[:status]).must_equal :failure
-    #
-    #   must_respond_with :bad_request
-    # end
-    #
-    # it "renders 400 bad_request for bogus categories" do
-    #   INVALID_CATEGORIES.each do |category|
-    #     new_work_params[:work][:category] = category
-    #
-    #     expect {
-    #       post works_path, params: new_work_params
-    #     }.wont_change 'Work.count'
-    #
-    #     expect(flash[:status]).must_equal :failure
-    #
-    #     must_respond_with :bad_request
-    #   end
-    # end
+
+
+      it "returns an error for invalid movie data" do
+        # arrange
+        movie_data[:title] = nil
+
+        expect {
+        post movies_path, params: { movie: movie_data }
+      }.wont_change "Movie.count"
+
+        body = JSON.parse(response.body)
+
+        body = check_response(expected_type: Hash, expected_status: :bad_request)
+        expect(body).must_include 'errors'
+      end
 
   end
 end
